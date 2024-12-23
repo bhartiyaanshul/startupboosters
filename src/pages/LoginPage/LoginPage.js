5b8fd8532d1a24d9fbc467f69a5767f8cdcf9c24
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-12-23 12:21:31 +0530

    Login & signup Page created

---
+++ src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { createClient } from '@supabase/supabase-js'
+import './LoginPage.css'
+
+const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_ANON_KEY)
+
+export const LoginPage = () => {
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleEmail = (e) => setEmail(e.target.value)
+
+    const handlePassword = (e) => setPassword(e.target.value)
+
+    async function handleLogin() {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        })
+        console.log(error)
+
+        if (!data.user) {
+            alert('Invalid login credentials')
+        }
+        else {
+            navigate("/")
+        }
+    }
+  return (
+    <div className='login-page'>
+        <div className='content-container'>
+            <div className='signin-container'>
+                <div className='sign-in-text'>Sign In</div>
+                <input type='text' onChange={handleEmail} value={email} placeholder='Email' required></input>
+                <input type='password' onChange={handlePassword} value={password} placeholder='Password'></input>
+                <button type='submit' onClick={handleLogin} >LOGIN</button>
+                <div>Don't have Account? <Link to='/signup'>SignUp</Link></div>
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/SignupPage/SignupPage.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { createClient } from '@supabase/supabase-js'
+import { useState } from 'react'
+import './SignupPage.css'
+
+const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_ANON_KEY)
+
+export const SignupPage = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    function handleEmail(e) {
+        setEmail(e.target.value)
+    }
+
+    function handlePassword(e) {
+        setPassword(e.target.value)
+    }
+
+    function handleConfirmPassword(e) {
+        setConfirmPassword(e.target.value)
+    }
+    async function handleSignUp() {
+
+        if(password === confirmPassword){
+            const { data, error } = await supabase.auth.signUp({
+                email: email,
+                password: password,
+            })
+            console.log(data,error)
+            alert("Verification mail has been send and then login")
+        }
+        else{
+            alert("Password & Confirm Password didn't match")
+        }
+    }
+  return (
+    <div>
+        <div className='content-container'>
+            <div className='signin-container'>
+                <div className='sign-in-text'>Sign Up</div>
+                <input type='text' onChange={handleEmail} value={email} placeholder='Email'></input>
+                <input type='password' onChange={handlePassword} value={password} placeholder='Password'></input>
+                <input type='password' onChange={handleConfirmPassword} value={confirmPassword} placeholder='Confirm Password'></input>
+                <button type='submit' onClick={handleSignUp} >Sign Up</button>
+                <div>Already SignUp? <Link to='/login'>Login</Link></div>
+            </div>
+        </div>
+    </div>
+  )
+}
