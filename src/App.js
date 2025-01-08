f471b8c8f2441d4671f658515b46ba1ce332065c
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2025-01-08 19:48:39 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { LoginPage } from './pages/LoginPage/LoginPage';
 import { SignupPage } from './pages/SignupPage/SignupPage';
-import { HomePage } from './pages/HomePage/HomePage';
-import { Navbar } from './components/Navbar/Navbar'
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,24 +1,22 @@
 import React from 'react'
-import './HomePage.css'
 import { Navbar } from '../../components/Navbar/Navbar'
 import { IntroSection } from '../../components/IntroSection/IntroSection'
-import { Features } from '../../components/Features/Features'
-import { ServicesSection } from '../../components/ServicesSection/ServicesSection'
-import { Feedback } from '../../components/Feedback/Feedback'
 import Accordion from '../../components/Accordion/Accordion'
 import { LetsStartSection } from '../../components/LetsStartSection/LetsStartSection'
 import { Footer } from '../../components/Footer/Footer'
 import Explore from '../../components/Explore/Explore'
 
-export const HomePage = () => {
+const HomePage = () => {
   return (
     <div className='landing-page'>
       <Navbar />
       <IntroSection />
       <Explore />
-      <Accordion/>
-      <LetsStartSection  />
+      <Accordion />
+      <LetsStartSection />
       <Footer />
     </div>
   )
 }
+
+export default HomePage
